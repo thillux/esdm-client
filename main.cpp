eb235492b13091f604f32bd9edd21eb11dec9503
Markus Theil <theil.markus@gmail.com>
2023-06-14 17:51:48 +0200

    play with entropy adding
    Signed-off-by: Markus Theil <theil.markus@gmail.com>

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ int main(void) {
   std::cout << "ESDM Client" << std::endl;
 
   esdm_rpcc_set_max_online_nodes(1);
-  /* Return code irrelevant due to fallback in functions below */
   esdm_rpcc_init_unpriv_service(NULL);
 
   int ret = 0;
@@ -19,9 +18,9 @@ int main(void) {
   esdm_invoke(esdm_rpcc_status(statusStr.data(), statusStr.size()));
   std::cout << statusStr.data() << std::endl;
 
-  std::array<uint8_t, 8> randBytes;
+  std::array<uint8_t, 32> randBytes;
   esdm_invoke(
-      esdm_rpcc_get_random_bytes_pr(randBytes.data(), randBytes.size()));
+      esdm_rpcc_get_random_bytes_full(randBytes.data(), randBytes.size()));
 
   std::cout << "0x";
   for (size_t i = 0; i < randBytes.size(); ++i) {
@@ -30,6 +29,14 @@ int main(void) {
   std::cout << std::endl;
 
   esdm_rpcc_fini_unpriv_service();
+
+  esdm_rpcc_init_priv_service(NULL);
+
+  std::cout << "Warning: this is cyclic entropy feeding for testing purposes and should never be done in production!" << std::endl;
+  esdm_invoke(esdm_rpcc_rnd_add_entropy(randBytes.data(), randBytes.size(), randBytes.size() * 8));
+  assert(ret == 0);
+
+  esdm_rpcc_fini_priv_service();
 
   return 0;
 }
