d6a400115112c71c75a09cf0c1d07d1efad2d665
Markus Theil <theil.markus@gmail.com>
2023-06-28 10:23:05 +0200

    update test tool
    Signed-off-by: Markus Theil <theil.markus@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,22 +5,16 @@
 #include <cstring>
 #include <esdm/esdm_rpc_client.h>
 #include <iostream>
+#include <sys/types.h>
 
-int main(void) {
-  std::cout << "ESDM Client" << std::endl;
+void testUnprivRand() {
+  ssize_t ret = 0;
 
-  esdm_rpcc_set_max_online_nodes(1);
   esdm_rpcc_init_unpriv_service(NULL);
-
-  int ret = 0;
-  std::array<char, 8192> statusStr;
-  memset(statusStr.data(), 0, statusStr.size());
-  esdm_invoke(esdm_rpcc_status(statusStr.data(), statusStr.size()));
-  std::cout << statusStr.data() << std::endl;
 
   std::array<uint8_t, 32> randBytes;
   esdm_invoke(
-      esdm_rpcc_get_random_bytes_full(randBytes.data(), randBytes.size()));
+      esdm_rpcc_get_random_bytes_pr(randBytes.data(), randBytes.size()));
 
   std::cout << "0x";
   for (size_t i = 0; i < randBytes.size(); ++i) {
@@ -29,14 +23,51 @@ int main(void) {
   std::cout << std::endl;
 
   esdm_rpcc_fini_unpriv_service();
+}
+
+void testPrivRand() {
+  ssize_t ret = 0;
 
   esdm_rpcc_init_priv_service(NULL);
 
+  std::array<uint8_t, 32> randBytes;
   std::cout << "Warning: this is cyclic entropy feeding for testing purposes and should never be done in production!" << std::endl;
   esdm_invoke(esdm_rpcc_rnd_add_entropy(randBytes.data(), randBytes.size(), randBytes.size() * 8));
   assert(ret == 0);
 
   esdm_rpcc_fini_priv_service();
+}
+
+void testSeed() {
+   esdm_rpcc_init_unpriv_service(NULL);
+
+  ssize_t ret = 0;
+
+  std::array<uint64_t, 1024 / sizeof(uint64_t)> randBytes;
+  esdm_invoke(
+      esdm_rpcc_get_seed(reinterpret_cast<uint8_t*>(randBytes.data()), randBytes.size() * sizeof(uint64_t), 0));
+  std::cout << "Ret: " << ret << std::endl;
+
+  std::cout << "returned buffer size: " << randBytes[0] << std::endl;
+  std::cout << "returned collected bits: " << randBytes[1] << std::endl;
+
+  std::cout << "0x";
+  for (size_t i = 2; i < randBytes[0]; ++i) {
+    std::cout << boost::format("%02x") % static_cast<int>(reinterpret_cast<uint8_t*>(&randBytes[0])[i]);
+  }
+  std::cout << std::endl;
+
+  esdm_rpcc_fini_unpriv_service();
+}
+
+int main(void) {
+  std::cout << "ESDM Client" << std::endl;
+
+  esdm_rpcc_set_max_online_nodes(1);
+  
+  //testPrivRand();
+  //testSeed();
+  testUnprivRand();
 
   return 0;
 }
