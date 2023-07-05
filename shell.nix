507ec6eef4b0e8c64df804fa8aafc66ae8eda95c
Oliver Richter <oliver.richter@secunet.com>
2023-07-05 11:46:52 +0200

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ sources ? import nix/sources.nix }:
+
+let
+  sources = import nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [ esdm protobufc boost ];
+  nativeBuildInputs = with pkgs; [ meson ninja pkgconfig cmake ];
+
+   # https://github.com/NixOS/nixpkgs/issues/86131
+  BOOST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.boost}/include";
+  BOOST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.boost}/lib";
+}
